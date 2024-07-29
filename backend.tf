1394f43e6378f71c0824c518e5daf1ae172e5771
Nathalie <natkes.2002@gmail.com>
2024-07-29 17:12:15 +0200

    terraform week10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "w7-nk-terraform"   #replace with your bucket
+    bucket = "noone"   #replace with your bucket
     key    = "week10/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
-    dynamodb_table = "locktable"
+    dynamodb_table = "none"
   }
 }
